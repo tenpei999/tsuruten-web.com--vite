25b7d66745f40deeb7c23e1e2db8949f6a16bd93
tenpei999 <tenpei999@gmail.com>
2023-03-29 22:41:04 +0900

    [feat]css反映成功

--- functions.php
+++ functions.php
@@ -1,91 +1,88 @@
 <?php
 
-function custom_theme_support()
-{
-  add_theme_support('html5', array(
-    'menus',
-    'title-tag',
-    'search-form',
-    'gallery',
-    'caption',
-  ));
-  add_theme_support('post-thumbnails'); // 投稿画像の表示
-  add_theme_support('title-tag');
-  add_theme_support('responsive-embeds');
-  add_theme_support('custom-background');
-  add_theme_support('automatic-feed-links');
-  register_nav_menus(array(
-    'category_nav' => esc_html__('category navigation', 'tsuruten-web'),
-  ));
-  add_theme_support('editor-styles');
-  add_editor_style();
+/**
+ * Functions and definitions
+ *
+ * @package WordPress
+ * @subpackage themrishvite
+ */
+
+//develop mode config
+define( "IS_VITE_DEVELOPMENT", true );
+
+//define
+define( 'DIST_DEF', 'dist' );
+define( 'DIST_URI',  get_template_directory_uri() . '/' . DIST_DEF );
+define( 'DIST_PATH', get_template_directory()     . '/' . DIST_DEF );
+
+define( 'JS_DEPENDENCY', array() ) ; // array( 'jquery' ) as example
+define( 'JS_LOAD_IN_FOOTER', true ) ; // load scripts in footer?
+
+define('VITE_SERVER', 'http://localhost:3000');
+define('VITE_ENTRY_POINT', '/main.js');
+
+/*
+ * init theme support
+ */
+function themrishvite_theme_support() {
+	add_theme_support( 'html5', array (
+		'comment-form',
+		'comment-list',
+		'search-form',
+		'gallery',
+		'caption',
+		'style',
+		'script'
+	) );
+	add_theme_support( "post-thumbnails" );
+	add_theme_support( 'title-tag' );
+	add_theme_support( 'editor-styles' );
+	add_theme_support( 'custom-logo' );
+	add_theme_support( 'automatic-feed-links' );
+	register_nav_menus( array (
+		'main-menu' => __( 'mainmenu', 'themrishvite' )
+	) );
 }
-add_action('after_setup_theme', 'custom_theme_support');
+add_action( 'after_setup_theme', 'themrishvite_theme_support' );
 
-
-//タイトル出力
-function change_title($title)
-{
-  if (is_front_page() && is_home()) { //トップページなら
-    $title = get_bloginfo('name', 'display');
-  } elseif (is_singular()) { //シングルページなら
-    $title = single_post_title('', false);
-  }
-  return $title;
+function cors_http_header() {
+	header( "Access-Control-Allow-Origin: *" );
 }
-add_filter('pre_get_document_title', 'change_title');
-
-function  readScript()
-{
-  wp_enqueue_script('bundle', get_theme_file_uri() . '/js/main.min.js', array(), '1.0.0', true);
-
-  $tmp_path_arr = [
-    'temp_uri' => get_template_directory_uri(),
-  ];
-
-  if (is_page('concept')) {
-    $script_path = '/js/concept.min.js';
-
-    wp_enqueue_script(
-      'bundle2',
-      get_theme_file_uri() . $script_path,
-      [],
-      '1.0.0',
-      true
-    );
-    wp_localize_script('bundle2', 'tmp_path', $tmp_path_arr);
-  }
-  if (is_front_page()) {
-    $script_path = '/js/front.min.js';
-
-    wp_enqueue_script(
-      'bundle2',
-      get_theme_file_uri() . $script_path,
-      [],
-      '1.0.0',
-      true
-    );
-    wp_localize_script('bundle2', 'tmp_path', $tmp_path_arr);
-  }
-  wp_enqueue_style('swiper', get_theme_file_uri() . '/swiper/css/swiper-bundle.min.css', array('cssStyle'), '1.0.0', false);
-  wp_enqueue_style('Noto+Sans+JP', '//fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;900&display=swap', array());
-  wp_enqueue_style('Noto+Serif+JP', '//fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200;300;400;500;600;700&display=swap', array());
-  wp_enqueue_style('Zen Maru Gothic', '//fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@400;500&display=swap', array());
-  wp_enqueue_style('M+PLUS+1', '//fonts.googleapis.com/css2?family=M+PLUS+1:wght@400;500;600&display=swap', array());
-  wp_enqueue_style('Shippori Antique', '//fonts.googleapis.com/css2?family=Shippori+Antique&display=swap', array());
-  wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css', array(), '4.7.0');
-  wp_enqueue_style('cssStyle', get_template_directory_uri() . '/css/style.css', array(), '1.0.0');
-  wp_enqueue_style('style', get_template_directory_uri() . '/style.css', array(), '1.0.0');
-}
-add_action('wp_enqueue_scripts', 'readScript');
+add_action( 'send_headers', 'cors_http_header' );
 
 
-function add_my_scripts()
-{
-  wp_enqueue_script('swiper-js', get_theme_file_uri() . '/swiper/swiper-bundle.min.js', array('bundle'), '1.0.0', true);
-  wp_enqueue_script('slider', get_theme_file_uri() . '/js/slider.min.js', array('swiper-js'), '1.0.0', true);
-}
-add_action('wp_enqueue_scripts', 'add_my_scripts');
+add_action( 'wp_enqueue_scripts', function() {
+	if ( defined( 'IS_VITE_DEVELOPMENT') && IS_VITE_DEVELOPMENT === true ) {
+		//develop mode
+		function vite_head_module_hook() {
+			echo '<script type="module" crossorigin src="' . VITE_SERVER . VITE_ENTRY_POINT . '"></script>';
+		}
+		add_action( 'wp_footer', 'vite_head_module_hook' );
+	} else {
+		// production mode, 'npm run build' must be executed in order to generate assets
+
+		// read manifest.json to figure out what to enqueue
+		$manifest = json_decode( file_get_contents( DIST_PATH . '/manifest.json'), true );
+		
+		// is ok
+		if ( is_array( $manifest ) ) {
+			
+			// get first key, by default is 'main.js'
+			$manifest_key = array_keys( $manifest );
+			if ( isset( $manifest_key[0] ) ) {
+				// enqueue CSS files
+				foreach( @$manifest["main.css"] as $css_file ) {
+					wp_enqueue_style( 'main', DIST_URI . '/' . $css_file );
+				}
+				// enqueue main JS file
+				$js_file = @$manifest[$manifest_key[0]]['file'];
+				if ( ! empty( $js_file ) ) {
+					wp_enqueue_script( 'main', DIST_URI . '/' . $js_file, JS_DEPENDENCY, '', JS_LOAD_IN_FOOTER );
+				}
+			}
+		}
+	}
+} );
 
 /* ウイジェット追加
 ---------------------------------------------------------- */
